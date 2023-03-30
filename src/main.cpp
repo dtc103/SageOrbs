c88d750fa494e2e7909977f239078bfc90acad95
dtc103 <jan_kerner@gmx.de>
2023-03-30 16:04:38 +0200

    added interpolation cycle

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,14 @@
 4. from 014a4f to 52fffd
 45d9cb
 1e6a7d
+
+Color values
+1. 52fffd = rgb(82, 255, 253)
+2. 014a4f = rgb(1, 74, 79)
+3. 08674f = rgb(8, 103, 79)
+4. 014a4f = rgb(1, 74, 79)
 */
+
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
@@ -23,29 +30,24 @@ struct Color{
 
 
 
-Color interpolateColor(Color in, Color target, int step, int intervallSize){
+Color interpolateColor(Color &in, Color &target, int step, int intervallSize){
 	double fraction = (double)step/(double)intervallSize;
 	return Color(
 		in.r + (target.r - in.r) * fraction, 
-	in.g + (target.g - in.g) * fraction, 
-	in.b + (target.b - in.b) * fraction,
-	in.alpha + (target.alpha - in.alpha) * fraction
+		in.g + (target.g - in.g) * fraction, 
+		in.b + (target.b - in.b) * fraction,
+		in.alpha + (target.alpha - in.alpha) * fraction
 	);
 }
 
-void orbEffect(Adafruit_NeoPixel p, int ledCount){
+void orbEffect(Adafruit_NeoPixel &p, int ledCount){
 	int colorCount = 4;
-	/*
-	Color values
-	1. 52fffd = rgb(82, 255, 253)
-	2. 014a4f = rgb(1, 74, 79)
-	3. 08674f = rgb(8, 103, 79)
-	4. 014a4f = rgb(1, 74, 79)
-	*/
+	
+	//values taken from: see above
 	Color colors[colorCount] = {Color(82, 255, 253), Color(1, 74, 79), Color(8, 103, 79), Color(1, 74, 79)};
 	const int singleIntervall = 1000;
 	const int intervallSize = colorCount * singleIntervall;
-	
+	 
 	int stepValues[ledCount] = {0};
 	for(int i = 0; i < ledCount; ++i){
 		stepValues[i] = random(intervallSize);
@@ -53,8 +55,27 @@ void orbEffect(Adafruit_NeoPixel p, int ledCount){
 
 	for(int step = 0; step <= intervallSize; ++i){
 		for(int led = 0; led < ledCount; ++led){
+			Color currColor;
+			switch(stepValues[led] % 1000){
+				case 0: 
+					currColor = interpolateColor(colors[0], colors[1], step, singleIntervall);
+					break;
+				case 1:
+					currColor = interpolateColor(colors[1], colors[2], step, singleIntervall);
+					break;
+				case 2:
+					currColor = interpolateColor(colors[2], colors[3], step, singleIntervall);
+					break;
+				case 3:
+					currColor = interpolateColor(colors[3], colors[0], step, singleIntervall);
+					break;
+			}
+			stepValues[led] = (stepValues[led] + 1) % intervallSize;
+
+			p.setPixelColor(led, currColor.r, currColor.g, currColor.b);
 			
 		}
+		p.show();
 	}
 }
 
@@ -75,6 +96,7 @@ void setup() {
 
 void loop() {
 
+	orbEffect(p, 6);
 
 	/*
 	Serial.println("Start: " + String(start.r) + " " + String(start.g) + " " + String(start.b));
